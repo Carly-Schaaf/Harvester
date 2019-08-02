d37e1134c2dcb9389528918bcdd6a478a2bfcb1b
Carly Schaaf <carlyschaaf@berkeley.edu>
2019-08-01 17:16:50 -0700

    finish making icons

--- frontend/src/components/produces/produceIndex.jsx
+++ frontend/src/components/produces/produceIndex.jsx
@@ -11,15 +11,43 @@ import Divider from '@material-ui/core/Divider';
 import { Typography } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/styles';
-import Grid from '@material-ui/core/Grid';
+import NaturePeople from '@material-ui/icons/NaturePeople';
+import AccessibleForward from '@material-ui/icons/AccessibleForward';
+import Add from '@material-ui/icons/Add';
+import HighQuality from '@material-ui/icons/HighQuality';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Slide from '@material-ui/core/Slide';
+
 
 const styles = theme => ({
     root: {
         "margin-bottom": "20px",
+        display: "flex",
+        "align-items": "baseline",
+        '&:hover': {
+            background: "antiquewhite",
+        }
     },
     listItem: {
         "display": "flex",
-        "flex-direction": "column"
+        "flex-direction": "column",
+        width: "50%",
+        "padding-top": "0px"
+    },
+    typography: {
+        width: "100px"
+    },
+    icon: {
+        fontSize: "15px",
+        position: 'relative',
+        top: '2px',
+        "margin-right": "5px"
+    },
+    gridItem: {
+        display: "flex",
+        "align-items": "baseline",
+        margin: "5px 0"
     }
 });
 
@@ -45,44 +73,56 @@ class ProduceIndex extends React.Component {
     }
 
     renderFeatures(args) {
-        const { abundance, accessible, ownerPermission, quality} = args;
+        const { abundance, accessible, ownerPermission, quality } = args;
+        const { classes } = this.props;
         const publicLand = args.public;
-        const adjectiveMap = {
-            5: "Extremely",
-            4: "Very",
-            3: "Decently",
-            2: "Not-so",
-            1: "Not"
-        }
         let publicOrPrivate;
         if (publicLand) {
-            publicOrPrivate = "Totally public"
+            publicOrPrivate = "Public"
         } else {
-            publicOrPrivate = `${adjectiveMap[ownerPermission]} cool with the owner`
+            publicOrPrivate = "Owner Consent"
         }
+
+        const features = Object.assign({ 
+            "abundant": abundance, 
+            accessible, 
+            "quality stuff": quality }, 
+            {[publicOrPrivate]: "publicOrPrivate"});
+
+        const gridItems = Object.keys(features).map((feature) => {
+            if (features[feature] > 2 || (features[feature] === "publicOrPrivate")) {
+                let icon;
+                switch (feature) {
+                    case "abundant":
+                        icon = (<Add className={classes.icon} />);
+                        break;
+                    case "accessible":
+                        icon = (<AccessibleForward className={classes.icon} />);
+                        break;
+                    case "quality stuff":
+                        icon = (<HighQuality className={classes.icon} />);
+                        break;
+                    case "Public":
+                        icon = (<NaturePeople className={classes.icon} />);
+                        break;
+                    case "Owner Consent":
+                        icon = (<NaturePeople className={classes.icon} />);
+                        break;
+                }
+                return(
+                    <div className="grid-item-container">
+                        {icon}
+                        <Typography className={classes.typography} variant="overline">
+                            {feature} 
+                        </Typography>
+                    </div>
+                )
+            }
+        })
         return(
-            <Grid container spacing={0}>
-                <Grid item zeroMinWidth xs={6}>
-                    <Typography variant="overline">
-                        {adjectiveMap[abundance]} abundant
-                    </Typography>
-                </Grid>
-                <Grid item zeroMinWidth xs={6}>
-                    <Typography variant="overline">
-                        {adjectiveMap[accessible]} accessible
-                    </Typography>
-                </Grid>
-                <Grid item zeroMinWidth xs={6}>
-                    <Typography variant="overline">
-                        {publicOrPrivate}
-                    </Typography>
-                </Grid>
-                <Grid item zeroMinWidth xs={6}>
-                    <Typography variant="overline">
-                        {adjectiveMap[quality]} high-quality stuff
-                    </Typography>
-                </Grid>
-            </Grid>
+            <div className="features-container">
+                {gridItems}
+            </div>
             
         )
     }
@@ -92,24 +132,23 @@ class ProduceIndex extends React.Component {
         let plural;
         let count = this.props.produce.length;
         count > 1 ? plural = " types of" : "";
-
         const listItems = this.props.produce.map(({ id, name, type, reviews, ...rest }, i) => (
             <Paper key={id + i} square className={classes.root}>
                 <ListItem className={classes.listItem} alignItems="flex-start">
                     <ListItemText primary={name} secondary={type} />
-                    {this.renderFeatures({...rest})}
                 </ListItem>
+                {this.renderFeatures({...rest})}
             </Paper>
         ));
-        return( 
+        return (
             <div>
                 <Typography align="center" variant="h4">
-                    {count} harvestable{plural} produce
-                </Typography>
-                <Divider className={classes.root} />
-                <List>
-                    {listItems}
-                </List>
+                        {count} harvestable{plural} produce
+                    </Typography>
+                    <Divider className={classes.root} />
+                    <List>
+                        {listItems}
+                    </List>
             </div>
         )
                 
